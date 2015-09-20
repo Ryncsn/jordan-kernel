e4507ff44a93941450be25789741a8e24c7c3390
ryncsn <ryncsn@gmail.com>
2015-09-20 10:40:43 +0800

    Revert "fix restart wifi, not good, but works"
    This reverts commit d3abd523eb45b63050f24a23552050419b62c654.

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1597,10 +1597,6 @@ int mmc_power_save_host(struct mmc_host *host)
 
 	mmc_power_off(host);
 
-//Quarx FIXME Pleaseee
-//Allow to restart wifi
-	mmc_reinit_host(host);
-//
 	return ret;
 }
 EXPORT_SYMBOL(mmc_power_save_host);
